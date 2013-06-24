a73efe0a16e3d695d04400fdbe38a3122dabfe73
Poyi <poyi.design@gmail.com>
2013-06-23 17:39:04 -0700

    Finished styling the project detail page, next work on project list page then guest view

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,6 @@ class Project < ActiveRecord::Base
 	has_many :notes, :dependent => :destroy
 	has_many :prototypes, :dependent => :destroy
 	has_many :designs, :dependent => :destroy
-  attr_accessible :detail, :name, :notes_attributes
+  attr_accessible :detail, :name, :notes_attributes, :publish
   accepts_nested_attributes_for :notes, :prototypes, :designs, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623174842) do
+ActiveRecord::Schema.define(:version => 20130623191124) do
 
   create_table "designs", :force => true do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130623174842) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+    t.boolean  "publish"
   end
 
   add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
